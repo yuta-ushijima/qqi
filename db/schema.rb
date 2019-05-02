fbecf118fa0d0ea1ae2b4b2b62a0a9ff849d4196
Yuta Ushijima <register@yuta-u.com>
2019-05-02 22:44:37 +0900

    Generate User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_132506) do
+ActiveRecord::Schema.define(version: 2019_05_02_134410) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -20,4 +20,15 @@ ActiveRecord::Schema.define(version: 2019_05_02_132506) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_users_on_article_id"
+  end
+
+  add_foreign_key "users", "articles"
 end
