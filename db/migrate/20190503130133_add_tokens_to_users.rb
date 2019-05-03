50b86313879363b378ae748a7f1f4a14b1d789e6
Yuta Ushijima <register@yuta-u.com>
2019-05-03 22:25:22 +0900

    Correspond users table for devise_token_auth

---
+++ db/migrate/20190503130133_add_tokens_to_users.rb
@@ -0,0 +1,45 @@
+class AddTokensToUsers < ActiveRecord::Migration[5.2]
+  def change
+
+    ## Required
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+
+    ## Tokens
+    add_column :users, :tokens, :text
+
+    ## Database authenticatable
+    add_column :users, :encrypted_password, :string, null: false, default: ""
+
+    ## Recoverable
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :string
+    add_column :users,:allow_password_change, :boolean, default: false
+
+    ## Rememberable
+    add_column :users, :remember_created_at, :datetime
+
+    ## Trackable
+    add_column :users, :sign_in_count, :integer, null: false , default: 0
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
+    add_column :users, :current_sign_in_ip, :string
+    add_column :users, :last_sign_in_ip, :string
+
+    # the following will update your models so that when you run your migration
+
+    # updates the user table immediately with the above defaults
+    User.reset_column_information
+
+    # finds all existing users and updates them.
+    # if you change the default values above you'll also have to change them here below:
+    User.find_each do |user|
+      user.uid = user.email
+      user.provider = "email"
+      user.save!
+    end
+
+    # to speed up lookups to these columns:
+    add_index :users, [:uid, :provider], unique: true
+  end
+end
