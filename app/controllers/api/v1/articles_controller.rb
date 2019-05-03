04cdeb90872ca6f3b63edc8b3407f8e5d9ce5faa
Yuta Ushijima <register@yuta-u.com>
2019-05-03 17:48:15 +0900

    Implement show action of articles_controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::ArticlesController < ApplicationController
+  before_action :set_article, only: [:update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     articles = Article.published.includes(:user)
     render json: articles
@@ -9,7 +12,17 @@ class Api::V1::ArticlesController < ApplicationController
     render json: @article
   end
 
+  def show
+    @article = current_user ? current_user.articles.find(params[:id])
+                            : Article.published.find(params[:id])
+    render json: @article
+  end
+
   private
+
+    def set_article
+      @article = current_user.articles.find(params[:id])
+    end
 
     def article_params
       params.require(:article).permit(:title, :body, :user_id)
