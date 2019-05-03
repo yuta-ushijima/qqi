e5c9eeda121c99ac9bf92e53ce15237da9fcd8b8
Yuta Ushijima <register@yuta-u.com>
2019-05-03 16:58:05 +0900

    implement create action of articles_cotroller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::ArticlesController < ApplicationController
     articles = Article.published.includes(:user)
     render json: articles
   end
+
+  def create
+    @article = current_user.articles.create!(article_params)
+    render json: @article
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :body, :post_status, :user_id)
+    end
 end
