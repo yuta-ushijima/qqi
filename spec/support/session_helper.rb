7c03d3c1bec5a2446bbb2706f61b31608d18e064
Yuta Ushijima <register@yuta-u.com>
2019-05-11 02:25:16 +0900

    Modified method from sign_in to atuhentication_headers_for

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,7 +1,8 @@
 module SessionHelpers
 
-  def sign_in(session_params)
-    post(api_v1_user_session_path, params: session_params.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' })
-    JSON.parse(response.body)["data"]
+  # ログインに必要なヘッダー情報の取得
+  def authentication_headers_for(user)
+    user.create_new_auth_token
   end
+
 end
