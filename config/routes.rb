69b7363f16f9e8b89cb33dea44e01f0255e4c36c
Yuta Ushijima <register@yuta-u.com>
2019-05-18 18:14:03 +0900

    Add routing frontend and backend

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
 
   root 'homes#index'
 
+  # TODO: 下記のroutingがなかった際フロント側でリロードしてもエラーにならない方法を考える
   get '/sign_in',  to: 'homes#index'
   get '/articles', to: 'homes#index'
   get '/sign_up', to: 'homes#index'
+  get '/post_articles', to: 'homes#index'
   get '/complete_user_registration', to: 'homes#index'
+
   get '/activation', to: 'activations#index'
 
   namespace :api do
