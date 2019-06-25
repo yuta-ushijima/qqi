1d3072783362ee886cdd824b3e1b63cbd641c749
Yuta Ushijima <register@yuta-u.com>
2019-06-26 00:05:50 +0900

    Create templete of MyPageContainer

---
+++ app/javascript/packs/container/mypage_container.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="mypage-container">
+    <header_container></header_container>
+    <ul>
+      <h2>私が投稿した記事一覧</h2>
+      <li v-for="" :key="">
+        <div class="">{{}}</div>
+        <div class="">{{}}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script lang="ts">
+  import axios from "axios"
+  import { Vue, Component } from "vue-property-decorator"
+  import VueRouter from 'vue-router'
+  import Header_container from "packs/container/header_container.vue";
+
+  /* devise-auth-tokenで設定したヘッダー情報 */
+  const config = {
+    headers: {
+      'Authorization': 'Bearer',
+      'Access-Control-Allow-Origin': '*',
+      'access-token': localStorage.getItem('access-token'),
+      'client': localStorage.getItem('client'),
+      'uid': localStorage.getItem('uid')
+    }
+  }
+
+  Vue.use(VueRouter);
+
+  @Component({
+    components: { Header_container }
+  })
+
+  export default class MyPageContainer extends Vue {
+
+  }
+</script>
+
+<style lang="scss"></style>
