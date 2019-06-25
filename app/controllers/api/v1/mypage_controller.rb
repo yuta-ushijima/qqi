ff5bf76cfcd03c68294a0b4467d14a0f77449d82
Yuta Ushijima <register@yuta-u.com>
2019-06-26 02:12:47 +0900

    Create endpont of api/v1/mypage

---
+++ app/controllers/api/v1/mypage_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::MypageController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    articles = current_user.articles
+    render json: articles
+  end
+end
---
+++ spec/requests/api/v1/mypages_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+describe 'Api::V1::Mypages' do
+  describe "GET /api/v1/mypages" do
+    context "ユーザーがログインしているとき" do
+      10.times do
+        before do
+          create(:article, post_status: :published, user_id: current_user_id)
+        end
+      end
+      before { create_list(:article, 10, user_id: current_user_id) }
+      let!(:other_user) { create(:user) }
+      let!(:other_user_id) { other_user.id }
+      let!(:current_user) { create(:user) }
+      let!(:current_user_id) { current_user.id }
+
+
+      it "自分が投稿した、下書きを含む全ての記事のレコードが取得できること" do
+        get(
+          api_v1_my_articles_path,
+          headers: authentication_headers_for(current_user)
+        )
+        res = JSON.parse(response.body)
+        expect(res["data"].all? { |data| data["attributes"]["user_id"] === current_user_id} ).to be_truthy
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "下書き記事は他のユーザーから見えないこと" do
+        get(
+          api_v1_my_articles_path,
+          headers: authentication_headers_for(other_user)
+        )
+        res = JSON.parse(response.body)
+        expect(res["data"].length).to eq(0)
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+
+end
