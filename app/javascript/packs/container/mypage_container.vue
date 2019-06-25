62ee28c135fa753e1206c458d689b51ecc246cbf
Yuta Ushijima <register@yuta-u.com>
2019-06-26 02:16:43 +0900

    Modified MyPageContainer because of display post_status

--- app/javascript/packs/container/mypage_container.vue
+++ app/javascript/packs/container/mypage_container.vue
@@ -3,9 +3,10 @@
     <header_container></header_container>
     <ul>
       <h2>私が投稿した記事一覧</h2>
-      <li v-for="" :key="">
-        <div class="">{{}}</div>
-        <div class="">{{}}</div>
+      <li v-for="myArticle in myArticles" :key="myArticle.id">
+        <div class="article__title">title: {{myArticle.title}}</div>
+        <div class="article-body">body: {{myArticle.body}}</div>
+        <div class="article-status">status: {{myArticle.post_status}}</div>
       </li>
     </ul>
   </div>
@@ -36,8 +37,31 @@
   })
 
   export default class MyPageContainer extends Vue {
+    myArticles: String[] = []
 
+    async mounted(): Promise<void> {
+      await this.fetchMyArticles();
+    }
+
+    async fetchMyArticles(): Promise<void> {
+      await axios.get("/api/v1/my_articles", config).then((response) => {
+        response.data.data.map((myArticle: any) => {
+          this.myArticles.push(myArticle.attributes);
+        })
+      })
+    }
   }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  li {
+    list-style: none;
+  }
+  div:nth-of-type(3) {
+    margin: 20px 0;
+    border-bottom: rgba(0,153,255,0.5) dotted 2px;
+  }
+  .article__title {
+    font-size: 1.5em;
+  }
+</style>
