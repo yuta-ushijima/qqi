a6ae61994dd3309a1c39512f71f14dfeb147eab1
Yuta Ushijima <register@yuta-u.com>
2019-06-30 17:43:17 +0900

    Modfied style of SignUpContainer by vuetify

--- app/javascript/packs/container/SignUpContainer.vue
+++ app/javascript/packs/container/SignUpContainer.vue
@@ -1,39 +1,77 @@
 <template>
-  <div class="container">
-    <header_container></header_container>
-    <h2 class="heading">ユーザー登録</h2>
-    <form class="login__form" v-on:submit.prevent="signUp">
-      <div>
-        <label>ユーザー名</label>
-        <input type="text" name="name" placeholder="ユーザー名を入力" v-model="name">
-      </div>
-        <div>
-        <label>メールアドレス</label>
-        <input type="text" name="email" placeholder="メールアドレスを入力" v-model="email">
-      </div>
-      <div>
-        <label>パスワード</label>
-        <input type="password" name="password" placeholder="パスワードを入力" v-model="password">
-      </div>
-      <button type="submit">登録</button>
-    </form>
-  </div>
+  <form>
+    <v-text-field
+      v-model="name"
+      label="ユーザー名"
+      data-vv-name="name"
+      v-validate="'required|max:10'"
+      :error-messages="errors.collect('name')"
+      :counter="10"
+      required
+
+    ></v-text-field>
+    <v-text-field
+      v-model="email"
+      label="メールアドレス"
+      data-vv-name="email"
+      v-validate="'required|email'"
+      :error-messages="errors.collect('email')"
+      required
+      autocomplete="off"
+    ></v-text-field>
+    <v-text-field
+      v-model="password"
+      label="パスワード"
+      data-vv-name="password"
+      v-validate="'required|min:8|max:50'"
+      :error-messages="errors.collect('password')"
+      :append-icon="show ? 'visibility' : 'visibility_off'"
+      :type="show ? 'text' : 'password'"
+      counter
+      hint="At least 8 characters"
+      @click:append="show = !show"
+      required
+      autocomplete="off"
+    ></v-text-field>
+
+    <v-btn @click="signUp" color="#55c500" class="white--text font-weight-bold">登録</v-btn>
+  </form>
 </template>
 
 <script lang="ts">
   import axios from "axios"
+  import VeeValidate from "vee-validate"
   import { Vue, Component } from "vue-property-decorator"
   import Router from '../router/router'
-  import Header_container from "./Header.vue";
 
-  @Component({
-    components: { Header_container }
-  })
+  Vue.use(VeeValidate, { locale: "ja" });
 
+  @Component
   export default class UserRegistrationContainer extends Vue {
-    public name: string = '';
-    public email: string = '';
-    public password: string = '';
+
+    $_veeValidate: {
+      validator: "new";
+    };
+
+    name: string = '';
+    email: string = '';
+    password: string = '';
+    show: boolean = false;
+    dictionary: {
+      attributes: {
+        email: "Email Addresses";
+      };
+      custom: {
+        name: {
+          required: () => "Name can not be empty";
+          max: "The name field may not be greater than 10 characters";
+        };
+      };
+    };
+
+    mounted() {
+      this.$validator.localize("ja", this.dictionary)
+    }
 
     async signUp(): Promise<void> {
 
@@ -53,55 +91,5 @@
 </script>
 
 <style lang="scss" scoped>
-  label {
-    margin-left: 0;
-  }
-  button {
-    display: block;
-    width: 150px;
-    margin: 20px auto 0;
-    padding: 15px;
-    cursor: pointer;
-  }
-  .heading {
-    position: relative;
-    display: inline-block;
-    margin: 30px 0 15px;
-    padding-bottom: 15px;
-    letter-spacing: 2px;
-    font-size: 3rem;
-    align-items: center;
-  }
-  .container {
-    text-align: center;
-  }
-  .heading::before,
-  .heading::after {
-    content: '';
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    border-bottom: 1px solid #999;
-  }
-  .heading::before {
-    bottom: 5px;
-  }
-  .login__form input[type=text],
-  .login__form input[type=password] {
-    width: 50%;
-    display: block;
-    margin: 10px auto;
-    padding: 15px;
-    border-radius: 0;
-  }
-  .login__form {
-    width: 50%;
-    margin: 100px auto;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    /*padding: 200px auto;*/
-    border: rgba(0,153,255,0.5) dotted 2px;
-    background-color: rgba(51,255,184,0.25);
-  }
+
 </style>
