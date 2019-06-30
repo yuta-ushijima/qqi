fa86351f8679302a9ab1e7c9c53f016416fa2a0d
Yuta Ushijima <register@yuta-u.com>
2019-06-30 22:40:06 +0900

    Modfied style of PostArticleContainer by vuetify

--- app/javascript/packs/container/PostArticleContainer.vue
+++ app/javascript/packs/container/PostArticleContainer.vue
@@ -1,38 +1,34 @@
 <template>
-  <div class="container">
-    <header_container></header_container>
-    <h2 class="heading">新規記事</h2>
-    <form class="article__form" v-on:submit.prevent="PostArticles">
-        <div>
-          <label>タイトル</label>
-          <input type="text" name="title" placeholder="タイトル" v-model="title">
-        </div>
-        <div>
-          <label>本文</label>
-          <textarea name="body"
-                    placeholder="プログラミング知識をMarkdown記法で書いて共有しよう"
-                    cols="30"
-                    rows="10"
-                    v-model="body"
-          >
-          </textarea>
-        </div>
-        <div class="article__form__selectBox">
-          <select class="dropdown" name="post_status" v-model="post_status">
-            <option selected value="draft">下書き保存</option>
-            <option value="published">公開する</option>
-          </select>
-        </div>
-        <button type="submit">投稿する</button>
+  <v-container class="article__form">
+    <form>
+      <v-text-field
+          outline
+          single-line
+          v-model="title"
+          name="title"
+          label="タイトル"
+          class="article__form__title"
+      ></v-text-field>
+      <v-textarea
+        outline
+        no-resize
+        height="100%"
+        v-model="body"
+        name="body"
+        label="プログラミング知識をMarkdown記法で書いて共有しよう"
+        class="article__form__body"
+      ></v-textarea>
+      <div class="text-xs-right">
+        <v-btn @click="PostArticles" color="#55c500" class="white--text font-weight-bold">QiitaClone2019に投稿</v-btn>
+      </div>
     </form>
-  </div>
+  </v-container>
 </template>
 
 <script lang="ts">
   import axios from "axios"
   import { Vue, Component } from "vue-property-decorator"
   import VueRouter from 'vue-router'
-  import Header_container from "./Header.vue";
 
   Vue.use(VueRouter);
 
@@ -46,10 +42,7 @@
     }
   }
 
-  @Component({
-    components: { Header_container }
-  })
-
+  @Component
   export default class PostArticlesContainer extends Vue {
     public title: string = '';
     public body: string = '';
@@ -72,58 +65,22 @@
   }
 </script>
 
-<style lang="scss">
-  .heading {
-    position: relative;
-    display: inline-block;
-    margin: 30px 0 15px;
-    padding-bottom: 15px;
-    letter-spacing: 2px;
-    font-size: 3rem;
-    align-items: center;
-  }
-  .container {
-    text-align: center;
-  }
-  .heading::before,
-  .heading::after {
-    content: '';
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    border-bottom: 1px solid #999;
-  }
-  .heading::before {
-    bottom: 5px;
-  }
+<style lang="scss"scoped>
   .article__form {
-    width: 90%;
-    margin: 100px auto;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    border: rgba(0,153,255,0.5) dotted 2px;
-    background-color: rgba(51,255,184,0.25);
-    display: block;
-    border-radius: 0;
-      input[type=text] {
-          width: 90%;
-          margin: 10px auto;
-          padding: 15px;
-      }
-      textarea {
-          width: 90%;
-          height: 500px;
-          display: block;
-          margin: 10px auto;
-          padding: 15px;
-      }
-      .article__form__selectBox {
-        padding: 20px;
-        select {
-            padding: 15px;
-        }
-      }
+    height: calc(100% - 64px - 10px);
+    display: flex;
+    flex-flow: column;
+    &__title {
+      flex: none;
+    }
+  }
+</style>
 
+<style lang="scss">
+  .article__form__body > .v-input__control {
+    height: 500px;
+  }
+  .v-text-field .v-text-field__details {
+    display: none;
   }
 </style>
