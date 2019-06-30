850010d2e8632a25f27568156414fb3235ef506b
Yuta Ushijima <register@yuta-u.com>
2019-06-30 20:19:28 +0900

    Modfied style of ArticleDetailContainer by vuetify

--- app/javascript/packs/container/ArticleDetailContainer.vue
+++ app/javascript/packs/container/ArticleDetailContainer.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="container">
-    <header_container></header_container>
-    <h2 class="heading">記事詳細</h2>
-      <div class="article">
-        <h1>{{articleTitle}}</h1>
-        <div class="article-body">{{articleBody}}</div>
-      </div>
-  </div>
+  <v-list two-line>
+    <v-container class="item elevation-3 article__container">
+      <v-layout></v-layout>
+      <v-layout>
+        <h1 class="article__container--title">{{articleTitle}}</h1>
+      </v-layout>
+      <v-layout>
+        <div class="article__container--body">{{articleBody}}</div>
+      </v-layout>
+    </v-container>
+  </v-list>
 </template>
 
 <script lang="ts">
   import axios from "axios"
   import { Vue, Component } from "vue-property-decorator"
   import VueRouter from 'vue-router'
-  import Header_container from "./Header.vue";
 
   Vue.use(VueRouter);
 
@@ -27,10 +29,7 @@
     }
   }
 
-  @Component({
-    components: { Header_container }
-  })
-
+  @Component
   export default class ArticleDetailContainer extends Vue {
     articleTitle: string = '';
     articleBody: string = '';
@@ -47,46 +46,15 @@
 </script>
 
 <style lang="scss">
-  .heading {
-    position: relative;
-    display: inline-block;
-    margin: 30px 0 15px;
-    padding-bottom: 15px;
-    letter-spacing: 2px;
-    font-size: 3rem;
-    align-items: center;
-  }
-  .container {
-    text-align: center;
-  }
-  .heading::before,
-  .heading::after {
-    content: '';
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    border-bottom: 1px solid #999;
-  }
-  .heading::before {
-    bottom: 5px;
-  }
-  .article {
-    width: 90%;
-    margin: 100px auto;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    border: rgba(0,153,255,0.5) dotted 2px;
-    background-color: rgba(51,255,184,0.25);
-    display: block;
-    border-radius: 0;
-    .article-body {
-      width: 90%;
-      height: 100%;
-      display: block;
-      background-color: #FFF;
-      margin: 10px auto;
-      padding: 15px;
+  .article__container {
+    margin-top: 1.5em;
+    &--title {
+      font-size: 2.5em;
+      line-height: 1.4;
+    }
+    &--body {
+      margin: 2em 0;
+      font-size: 16px;
     }
   }
 </style>
