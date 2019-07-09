7b263bc6e66f9ceace01b551d70deebf2ade5091
Yuta Ushijima <register@yuta-u.com>
2019-07-09 21:38:09 +0900

    Include all aseets must be precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,18 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-# For normalize.css
-Rails.application.config.assets.precompile += %w( normalize.css )
-Rails.application.config.assets.precompile += %w( global.css )
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    if full_path.starts_with? app_assets_path
+      logger.info "including asset: " + full_path
+      true
+    else
+      logger.info "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+end
