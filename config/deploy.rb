34b12dd3e6c33bb3047fdb5bb477b763d7d1cbc0
Yuta Ushijima <register@yuta-u.com>
2019-07-28 17:24:39 +0900

    fix: path of rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,7 @@ set :linked_files, %w[config/database.yml config/master.key]
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/packs", ".bundle", "node_modules"
 
 set :rbenv_type, :user
+set :rbenv_path, "/usr/local/rbenv"
 set :rbenv_ruby, "2.6.1"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w[rake gem bundle ruby rails]
